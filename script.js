fb4e2b5abdfa751310ccad85316394cfd4e8e39a
gitshubhsss <shubhamranjane16@gmail.com>
2024-04-11 10:56:42 +0530

    node js

---
+++ script.js
@@ -0,0 +1,5 @@
+import {sum,mul} from './math.js';
+import { generate} from "random-words";
+// console.log(`addtion is ${sum(4,5)}`);
+// console.log(`multiplication is ${mul(4,5)}`);
+console.log(generate());
